4e80698fbedee9c0adceff3074ddd91a881e85b3
Kaasiak <kasia.kobalczyk@gmail.com>
2021-09-15 09:42:29 +0100

    Clean and rename the script

--- R/respondents-data-generation.R
+++ R/respondents-data-generation.R
@@ -1,41 +1,6 @@
 #' This script generates the data frame for missing values
 library(tidyverse)
-
-# df_full <- read.table("data/unified_database_2014.tab", sep = "\t", header = TRUE)
-# ltdq <- read.table("data/ltdq.tab", sep = "\t", header = TRUE)
-# 
-# impute_missing <- function(x) {ifelse(x < 0, NA, x)}
-# 
-# df <- df_full %>% 
-#   select(ID, Cohort_all, sex = Sex_all, age = Age_all,
-#          freq1 = t1_a1,
-#          freq2 = t2_a1,
-#          freq3 = t3_a1,
-#          freq4 = t4_a1 ) %>%
-#   as.data.frame() %>%
-#   mutate(sex = fct_recode(factor(sex), F = "0", M = "1"),
-#          wave1 = 1,
-#          wave2 = if_else(freq2 == "-99", 0, 1),
-#          wave3 = if_else(freq3 == "-99", 0, 1),
-#          wave4 = if_else(freq4 == "-99", 0, 1)) %>%
-#   select(ID, wave1, wave2, wave3, wave4) %>%
-#   na.omit()
-# # 
-# # missing_df <- ltdq %>%
-# #   select(ID, cohort, sex, pass) %>%
-# #   filter(pass == 1) %>%
-# #   left_join(df)
-# # 
-# # missing_df %>%
-# #   filter(cohort == 9) %>%
-# #   select(cohort, pass, wave1, wave2, wave3, wave4) %>%
-# #   colSums(na.rm = TRUE)
-# 
-# 
-# write.csv(df, file = "data/missing_values.csv")
-
-
-# =======================
+library(xtable)
 
 ltdq <- read.table("../data/tab/ltdq.tab", sep = "\t", header = TRUE)
 deq1 <- read.table("../data/tab/deq1.tab", sep = "\t", header = TRUE)
@@ -60,15 +25,34 @@ missing_df <- ltdq %>%
          ) %>%
   select(sex, cohort, sent, wave1, wave2, wave3, wave4)
 
-# wave1 = all cohorts
-# wave2 = all cohorts
-# wave3 = cohorts 1-12
-# wave4 = cohorts 1-8
+#' wave1 = all cohorts, wave2 = all cohorts
+#' wave3 = cohorts 1-12, wave4 = cohorts 1-8
 
 missing_df[is.na(missing_df)] <- 0
 missing_df[missing_df$cohort > 12, "wave3"] <- NA
 missing_df[missing_df$cohort > 8, "wave4"] <- NA
 
-colSums(missing_df[,c("sent",waves)])
+# Respondents by cohort
+missing_cohort_df <- missing_df %>% 
+  group_by(cohort) %>% 
+  summarise(LTDQ = n(), DEQ = sum(sent), DEQ1 = sum(wave1), 
+            DEQ2 = sum(wave2), DEQ3 = sum(wave3), DEQ4 = sum(wave4)) %>%
+  mutate(cohort = LETTERS[cohort])
+missing_cohort_df[is.na(missing_cohort_df)] <- 0
+colSums(missing_cohort_df[,2:ncol(missing_cohort_df)])
+print(xtable(missing_cohort_df, type = "latex", digits = 0), include.rownames = FALSE)
 
+# Respondents by sex
+missing_sex_df <- missing_df %>% 
+  group_by(sex) %>% 
+  summarise(LTDQ = n(), DEQ1 = sum(wave1), 
+            DEQ2 = sum(wave2), DEQ3 = sum(wave3), DEQ4 = sum(wave4))  %>%
+  column_to_rownames("sex") %>% t() %>% as.data.frame()
+missing_sex_df[,"F_pct"] = round(missing_sex_df[,"F"] * 100 / rowSums(missing_sex_df), 1)
+missing_sex_df[,"M_pct"] = round(missing_sex_df[,"M"] * 100 / rowSums(missing_sex_df), 1)
+print(xtable(missing_sex_df[,c("F", "F_pct", "M", "M_pct")], type = "latex", digits = 1))
+
+# Save to csv
 write.csv(missing_df, file = "../data/processed/new_missing_values.csv")
+
+
