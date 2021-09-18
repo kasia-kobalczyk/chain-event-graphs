534da175b7e4e48746bebf7f6bbfc82d5660c9e9
Kaasiak <kasia.kobalczyk@gmail.com>
2021-09-18 10:58:36 +0100

    Adjust for unordered labeles of the nodes

--- python/tree2tex.py
+++ python/tree2tex.py
@@ -3,29 +3,27 @@ import os
 
 class converter(object):
     def __init__(self, params):
-        self.model = params.get('model')
-        self.labels = params.get('labels')
-        self.layer_nodes = params.get('layer_nodes')
+        self.figure = params.get('figure')
+        self.labelled_nodes = params.get('labelled_nodes')
         self.filename = params.get('filename')
 
     def export_to_tex(self):
-        mod_str = self.model.to_string()
+        mod_str = self.figure.to_string()
         f = open("figures/%s.dot" %self.filename, "w+")
         f.write(mod_str)
         f.close()
         cmd = "dot2tex -tmath --figonly figures/%s.dot > figures/%s.tex" % (self.filename, self.filename)
         os.system(cmd)
     
-    def __construct_labels(self, x_coordinates):
+    def __construct_labels(self):
         lines = []
         lines.append("\\begin{scope}\n")
-        for l, x in zip(self.labels, x_coordinates):
-            lines.append("  \draw (%sbp, -10bp) node {%s};\n" %(x, l))
+        for l in self.labelled_nodes.values():
+            lines.append("  \draw (%sbp, -10bp) node {%s};\n" %(l[1], l[0]))
         lines.append("\\end{scope}\n")
         return lines
     
     def postprocess_tex(self):
-        x_coordinates = []
         f = open("figures/" + self.filename + ".tex")
         lines = f.readlines()
         f.close()
@@ -39,12 +37,13 @@ class converter(object):
             if "\pgfsetlinewidth{1bp}" in l:
                 l = "  \pgfsetlinewidth{0.2bp}\n"
 
-            if "\draw" in l and "_{%d}$" %self.layer_nodes[layer] in l:
-                layer += 1
-                x_coordinates.append(re.search("\([0-9]+.[0-9]", l).group(0)[1:])
+            if "\draw" in l and any("_{%d}$" %k in l for k in self.labelled_nodes.keys()):
+                node = int(re.search("\_\{[0-9]+", l).group(0)[2:])
+                x_coordinate = re.search("\([0-9]+.[0-9]", l).group(0)[1:]
+                self.labelled_nodes[node] =  (self.labelled_nodes[node], x_coordinate)
 
             if "\\end{tikzpicture}" in l:
-                labels_tex = self.__construct_labels(x_coordinates)
+                labels_tex = self.__construct_labels()
                 f.writelines(labels_tex)
 
             f.write(l)
